91d2a05454d33f4d8407526059312df4be231c97
shobhit-28 <shobhitraj34@gmail.com>
2022-09-25 00:10:15 +0530

    added minion api

--- JS/brain.js
+++ JS/brain.js
@@ -1,1 +1,20 @@
 // alert("link check")
+
+var input = document.querySelector('textarea[name="input"]');
+var btn = document.querySelector('.btn');
+var output = document.querySelector('textarea[name="output"]');
+
+btn.addEventListener('click', clickHandler);
+
+function errorHandler(error){
+    alert("Error Ocurred \n" + error)
+}
+
+function clickHandler(){
+    var url = "https://api.funtranslations.com/translate/minion.json?text=";
+    text = url+input.value
+    fetch(text)
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(errorHandler)
+}
